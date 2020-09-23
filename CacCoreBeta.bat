f24fdb5f836c7c710519fe03bfa03665796a8f35
TanRayCz <45832275+TanRayCz@users.noreply.github.com>
2020-09-23 12:10:38 +0200

    Update CacCoreBeta.bat

--- CacCoreBeta.bat
+++ CacCoreBeta.bat
@@ -3,11 +3,11 @@ cls
 set EXE=SteamService.exe
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% goto READY
 if exist "C:\Program Files (x86)\Steam\steam.exe" goto DEFAULT
+if exist "memory.txt" goto CUSTOM
 color 6
 echo.
 echo SEARCHING FOR STEAM DIRECTORY ON C:\ DRIVE, PLEASE WAIT
-if exist "memory.txt" goto CUSTOM
-where /r C:\ steam.exe > memory.txt
+where /r C:\ steam.exe > memory.txt || goto FINDFAIL
 color 2
 echo.
 echo STEAM FOUND AT
@@ -46,6 +46,15 @@ echo FAILED TO LAUNCH STEAM, EXITING. PLEASE LAUNCH STEAM MANUALLY.
 timeout /t 5
 exit
 
+:FINDFAIL
+IF EXIST "memory.txt" DEL /Q "memory.txt"
+color 4
+cls
+echo.
+echo FAILED TO FIND STEAM, EXITING. PLEASE ENTER STEAM PATH IN memory.txt
+timeout /t 10
+exit
+
 :READY2
 cls
 color 2
@@ -64,7 +73,9 @@ goto START
 
 :START
 title Arma 3 CAC Launcher
-echo VERSION: BETA
+
+echo VERSION: 1.2.3
+
 echo.
 echo Choose CAC Server
 echo.
@@ -87,27 +98,27 @@ IF ERRORLEVEL 3 GOTO Coop
 IF ERRORLEVEL 2 GOTO ExileTanoa
 IF ERRORLEVEL 1 GOTO ExileAltis
 :ExileAltis
-start "" /normal arma3_x64 -connect=89.103.168.25 -port=2302 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank"
+start "" /normal arma3_x64 -connect=89.176.229.157 -port=2302 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank"
 GOTO End
 :ExileTanoa
-start "" /normal arma3_x64 -connect=89.103.168.25 -port=2602 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank"
+start "" /normal arma3_x64 -connect=89.176.229.157 -port=2602 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank"
 GOTO End
 :Coop
-start "" /normal arma3_x64 -connect=89.103.168.25 -port=2702 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Ace3;Mods\@CBA_A3;Mods\@EnhancedMovement;Mods\@MfHealAbort;Mods\@Vindicta;Mods\@ShackTacUI;Mods\@CHViewDistance;Mods\@VET_Unflipping;Mods\@TaskForceRadio"
+start "" /normal arma3_x64 -connect=89.176.229.157 -port=2702 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Ace3;Mods\@CBA_A3;Mods\@EnhancedMovement;Mods\@MfHealAbort;Mods\@Vindicta;Mods\@ShackTacUI;Mods\@CHViewDistance;Mods\@VET_Unflipping;Mods\@TaskForceRadio"
 GOTO End
 :ExileTanoaZ
-start "" /normal arma3_x64 -connect=89.103.168.25 -port=2382 -skipIntro -noSplash -world=empty -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@ZombiesAndDemons;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank;Mods\@ARM;Mods\@ShackTacUI"
+start "" /normal arma3_x64 -connect=89.176.229.157 -port=2382 -skipIntro -noSplash -world=empty -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@ZombiesAndDemons;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank;Mods\@ARM;Mods\@ShackTacUI"
 GOTO End
 :RavageMalden
-start "" /normal arma3_x64 -connect=89.103.168.25 -port=2342 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@CBA_A3;Mods\@VA_Beta;Mods\@Ravage;Mods\@ACE_No_medical;Mods\@VET_Unflipping;Mods\@EnhancedMovement;Mods\@ShackTacUI;Mods\@ARM"
+start "" /normal arma3_x64 -connect=89.176.229.157 -port=2342 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@CBA_A3;Mods\@VA_Beta;Mods\@Ravage;Mods\@ACE_No_medical;Mods\@VET_Unflipping;Mods\@EnhancedMovement;Mods\@ShackTacUI;Mods\@ARM"
 GOTO End
 :RavageTanoa
-start "" /normal arma3_x64 -connect=89.103.168.25 -port=2352 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@CBA_A3;Mods\@VA_Beta;Mods\@Ravage;Mods\@ACE_No_medical;Mods\@VET_Unflipping;Mods\@EnhancedMovement;Mods\@ShackTacUI;Mods\@ARM"
+start "" /normal arma3_x64 -connect=89.176.229.157 -port=2352 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@CBA_A3;Mods\@VA_Beta;Mods\@Ravage;Mods\@ACE_No_medical;Mods\@VET_Unflipping;Mods\@EnhancedMovement;Mods\@ShackTacUI;Mods\@ARM"
 GOTO End
 :ExileEscape
-start "" /normal arma3_x64 -connect=89.103.168.25 -port=2372 -skipIntro -noSplash -world=empty -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@ARM"
+start "" /normal arma3_x64 -connect=89.176.229.157 -port=2372 -skipIntro -noSplash -world=empty -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@ARM"
 GOTO End
 :KingofTheHill
-start "" /normal arma3_x64 -connect=89.103.168.25 -port=2322 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@EnhancedMovement;Mods\@MfHealAbort" 
+start "" /normal arma3_x64 -connect=89.176.229.157 -port=2322 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@EnhancedMovement;Mods\@MfHealAbort" 
 GOTO End
 :End
