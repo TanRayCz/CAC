c6b0d2af84a6ab9edc04ab6537b612d119cf1bc0
TanRayCz <45832275+TanRayCz@users.noreply.github.com>
2020-09-24 21:28:07 +0200

    Update CACLauncher.bat

--- CACLauncher.bat
+++ CACLauncher.bat
@@ -1,11 +1,3 @@
 @echo off
-set local=1.0
-set localtwo=%local%
-set link=https://raw.githubusercontent.com/TanRayCz/CAC/master/CACCore.bat
-:check
-IF EXIST "CACCore.bat" DEL /Q "CACCore.bat"
-goto :download
-pause
-:download
-download %link% CACCore.bat
-CALL "CACCore.bat"
+curl https://raw.githubusercontent.com/TanRayCz/CAC/master/CacCoreBeta.bat > CACCoreBeta.bat 2> nul
+call CACCore.bat
