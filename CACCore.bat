5eaede9adfe02de280dbf02881c0288107c297ce
TanRayCz <45832275+TanRayCz@users.noreply.github.com>
2020-09-27 18:09:54 +0200

    Update CACCore.bat

--- CACCore.bat
+++ CACCore.bat
@@ -93,7 +93,7 @@ goto START
 :START
 title Arma 3 CAC Launcher
 
-echo VERSION: 1.4.3b
+echo VERSION: 1.4.3c
 
 echo.
 echo Choose CAC Server
@@ -126,7 +126,7 @@ GOTO End
 start "" /normal arma3_x64 -connect=89.176.229.157 -port=2702 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Ace3;Mods\@CBA_A3;Mods\@EnhancedMovement;Mods\@MfHealAbort;Mods\@Vindicta;Mods\@ShackTacUI;Mods\@CHViewDistance;Mods\@VET_Unflipping;Mods\@TaskForceRadio"
 GOTO End
 :ExileTanoaZ
-start "" /normal arma3_x64 -connect=89.176.229.157 -port=2382 -skipIntro -noSplash -world=empty -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@ZombiesAndDemons;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank;Mods\@ARM;Mods\@ShackTacUI"
+start "" /normal arma3_x64 -connect=89.176.229.157 -port=3402 -skipIntro -noSplash -world=empty -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@ZombiesAndDemons;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank;Mods\@ARM;Mods\@ShackTacUI"
 GOTO End
 :RavageMalden
 start "" /normal arma3_x64 -connect=89.176.229.157 -port=2342 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@CBA_A3;Mods\@VA_Beta;Mods\@Ravage;Mods\@ACE_No_medical;Mods\@VET_Unflipping;Mods\@EnhancedMovement;Mods\@ShackTacUI;Mods\@ARM"
