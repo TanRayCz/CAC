32a424e9d4761e51c4892b886f7e7783d6512aa6
TanRayCz <45832275+TanRayCz@users.noreply.github.com>
2020-10-16 00:19:59 +0200

    Update CacCoreBeta.bat

--- CacCoreBeta.bat
+++ CacCoreBeta.bat
@@ -1,25 +1,27 @@
 @echo off
-cls
-if not exist "hosts.txt" curl https://raw.githubusercontent.com/TanRayCz/CAC/master/hosts.txt > hosts.txt 2> nul
+for /f "usebackq delims=" %%A in (CACCore\memory2.txt) do %%A
+curl https://raw.githubusercontent.com/TanRayCz/CAC/master/hosts.txt > hosts.txt 2> nul
 set EXE=SteamService.exe
+if not exist "CACCore" md "CACCore"
+if not exist CACCore\memory2.txt echo set Status=DISABLED > CACCore\memory2.txt
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% goto READY
 if exist "C:\Program Files (x86)\Steam\steam.exe" goto DEFAULT
-if exist "memory.txt" goto CUSTOM
+if exist "CACCore\memory.txt" goto CUSTOM
 color 6
 echo.
 echo SEARCHING FOR STEAM DIRECTORY, PLEASE WAIT
 (
    for %%a in ( a b c d e f g h i j k l m n o p q r s t u v w x y z ) do (
-      if exist "%%a:\" dir "%%a:\Steam.exe" /b /s /a-d
+      if exist "%%a:\" dir "%%a:\Steam.exe" /b /s /a-d 2> nul
    )
-)>"memory.txt"
+)>CACCore\memory.txt
 cls
-set /p SteamPath=<memory.txt
+set /p SteamPath=<CACCore\memory.txt
 if not exist "%SteamPath%" goto FINDFAIL
 color 2
 echo.
 echo STEAM FOUND AT
-type memory.txt
+type CACCore\memory.txt
 timeout /t 4
 goto CUSTOM
 
@@ -39,7 +41,7 @@ cls
 color 6
 echo.
 echo STEAM IS NOT RUNNING, ATTEMPTING TO RUN STEAM
-set /p SteamPath=<memory.txt
+set /p SteamPath=<CACCore\memory.txt
 start "" "%SteamPath%" -silent
 timeout /t 8
 cls
@@ -55,9 +57,8 @@ timeout /t 5
 exit
 
 :FINDFAIL
-IF EXIST "memory.txt" DEL /Q "memory.txt"
+IF EXIST "CACCore\memory.txt" DEL /Q "CACCore\memory.txt"
 cls
-color 6
 echo.
 echo FAILED TO FIND STEAM
 echo.
@@ -71,7 +72,7 @@ IF ERRORLEVEL 1 GOTO MANUAL
 cls
 echo. & echo Please enter path of your steam.exe. (Example: "C:\Steam.exe") & echo.
 set /p UserInput=Steam Directory: (Please use "" brackets): 
-(echo=%UserInput%) > "memory.txt"
+(echo=%UserInput%) > "CACCore\memory.txt"
 :FINDEXIT
 exit
 
@@ -92,10 +93,15 @@ echo.
 goto START
 
 :START
+color 2
 title Arma 3 CAC Launcher
-
-echo VERSION: 1.4.2a BETA
-
+echo VERSION: 1.5.0 unfinished untested
+echo CHANGES: CAC filesystem; status disable/enable (no implemented practical use so far); DNS instead of IP
+echo PLANNED: loop steam launch fail instead of exit; somewhat optimized mod selector (if %Status%==ENABLED); move KOTH 4
+echo potential .bat to cmd update, splitting github caclauncher in 2 for temporary legacy support
+echo.
+if %Status%==ENABLED echo OPTIONAL MODS: ENABLED
+if %Status%==DISABLED echo OPTIONAL MODS: DISABLED
 echo.
 echo Choose CAC Server
 echo.
@@ -104,44 +110,77 @@ echo  2 Exile Tanoa
 echo  3 Coop
 echo  4 Exile TanoaZ
 echo  5 Ravage Malden
-echo  6 Ravage Tanoa - OFFLINE
+echo  6 Exile Chernarus
 echo  7 Exile Escape
 echo  8 King of The Hill
 echo.
-choice /C 12345678 /M "Enter your server"
+echo  9 ENABLE/DISABLE Optional mods
+echo.
+choice /C 123456789 /M "Enter your server"
+IF ERRORLEVEL 9 GOTO StatusChanger
 IF ERRORLEVEL 8 GOTO KingofTheHill
 IF ERRORLEVEL 7 GOTO ExileEscape
-IF ERRORLEVEL 6 GOTO RavageTanoa
+IF ERRORLEVEL 6 GOTO Chernarus
 IF ERRORLEVEL 5 GOTO RavageMalden
 IF ERRORLEVEL 4 GOTO ExileTanoaZ
 IF ERRORLEVEL 3 GOTO Coop
 IF ERRORLEVEL 2 GOTO ExileTanoa
 IF ERRORLEVEL 1 GOTO ExileAltis
+
 :ExileAltis
-start "" /normal arma3_x64 -connect=89.176.229.133 -port=2302 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank"
+start "" /normal arma3_x64 -connect=cacservers.ddns.net -port=2302 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank"
 GOTO End
+
 :ExileTanoa
-start "" /normal arma3_x64 -connect=89.176.229.133 -port=2602 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank"
+start "" /normal arma3_x64 -connect=cacservers.ddns.net -port=2602 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank"
 GOTO End
+
 :Coop
-start "" /normal arma3_x64 -connect=89.176.229.133 -port=2702 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Ace3;Mods\@CBA_A3;Mods\@EnhancedMovement;Mods\@MfHealAbort;Mods\@Vindicta;Mods\@ShackTacUI;Mods\@CHViewDistance;Mods\@VET_Unflipping;Mods\@TaskForceRadio"
+start "" /normal arma3_x64 -connect=cacservers.ddns.net -port=2702 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Ace3;Mods\@CBA_A3;Mods\@EnhancedMovement;Mods\@MfHealAbort;Mods\@Vindicta;Mods\@ShackTacUI;Mods\@CHViewDistance;Mods\@VET_Unflipping"
 GOTO End
+
 :ExileTanoaZ
-start "" /normal arma3_x64 -connect=89.176.229.133 -port=2382 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@ZombiesAndDemons;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank;Mods\@ARM;Mods\@ShackTacUI"
+start "" /normal arma3_x64 -connect=cacservers.ddns.net -port=3402 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@ZombiesAndDemons;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@Extended_Base_Mod;Mods\@X66-MammothTank;Mods\@ARM;Mods\@ShackTacUI"
 GOTO End
+
 :RavageMalden
-start "" /normal arma3_x64 -connect=89.176.229.133 -port=2342 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@CBA_A3;Mods\@VA_Beta;Mods\@Ravage;Mods\@ACE_No_medical;Mods\@VET_Unflipping;Mods\@EnhancedMovement;Mods\@ShackTacUI;Mods\@ARM"
+start "" /normal arma3_x64 -connect=cacservers.ddns.net -port=2342 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@CBA_A3;Mods\@VA_Beta;Mods\@Ravage;Mods\@ACE_No_medical;Mods\@VET_Unflipping;Mods\@EnhancedMovement;Mods\@ShackTacUI;Mods\@ARM"
 GOTO End
-:RavageTanoa
-::start "" /normal arma3_x64 -connect=89.176.229.133 -port=2352 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@CBA_A3;Mods\@VA_Beta;Mods\@Ravage;Mods\@ACE_No_medical;Mods\@VET_Unflipping;Mods\@EnhancedMovement;Mods\@ShackTacUI;Mods\@ARM"
-cls
-echo. & echo Ravage Tanoa has been shutdown. Choose different server & echo.
-goto START
+
+:Chernarus
+start "" /normal arma3_x64 -connect=cacservers.ddns.net  -port=3302 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@CUPTerrainsCore;Mods\@CUPTerrainsMaps;Mods\@ChernarusIsles;Mods\@DSHouses;Mods\@InteriorsforCUP;Mods\@SM_Sheds;Mods\@CUPUnits;Mods\@CUPVehicles;Mods\@CUPWeapons;Mods\@Extended_Base_Mod;Mods\@DualArms;Mods\@AdvancedUrbanRappelling;Mods\@MfHealAbort;Mods\@EnhancedMovement;Mods\@JSRS_SOUNDMOD"
 GOTO End
+
 :ExileEscape
-start "" /normal arma3_x64 -connect=89.176.229.133 -port=2372 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@ARM"
+start "" /normal arma3_x64 -connect=cacservers.ddns.net -port=2372 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@Exile;Mods\@CBA_A3;Mods\@DualArms;Mods\@EnhancedMovement;Mods\@ARM"
 GOTO End
+
 :KingofTheHill
-start "" /normal arma3_x64 -connect=89.176.229.133 -port=2322 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@EnhancedMovement;Mods\@MfHealAbort" 
+start "" /normal arma3_x64 -connect=cacservers.ddns.net -port=2322 -skipIntro -noSplash -world=empty -exThreads=7 -enableHT "-mod=Mods\@EnhancedMovement;Mods\@MfHealAbort" 
 GOTO End
+
+:StatusChanger
+color 6
+cls
+echo  1 ENABLE OPTIONAL MODS
+echo  2 DISABLE OPTIONAL MODS
+echo.
+choice /C 12
+IF ERRORLEVEL 2 GOTO MODDISABLE
+IF ERRORLEVEL 1 GOTO MODENABLE
+
+:MODENABLE
+if exist CACCore\memory2.txt del CACCore\memory2.txt
+>>CACCore\memory2.txt Echo set Status=ENABLED
+set Status=ENABLED
+cls
+goto START
+
+:MODDISABLE
+if exist CACCore\memory2.txt del CACCore\memory2.txt
+>>CACCore\memory2.txt Echo set Status=DISABLED
+set Status=DISABLED
+cls
+goto START
+
 :End
