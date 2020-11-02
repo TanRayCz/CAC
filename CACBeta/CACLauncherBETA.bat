f1b1967b8c5a67671a59774561aeff3346b3f2e3
TanRayCz <45832275+TanRayCz@users.noreply.github.com>
2020-11-02 12:35:27 +0100

    Update CACLauncherBETA.bat

--- CACBeta/CACLauncherBETA.bat
+++ CACBeta/CACLauncherBETA.bat
@@ -1,3 +1,4 @@
 @echo off
+if not exist CACCore mkdir CACCore
 curl https://raw.githubusercontent.com/TanRayCz/CAC/master/CACBeta/CACCoreBeta.bat > CACCore\CACCoreBeta.bat 2> nul
 call CACCore\CACCoreBeta.bat
