670fe964f5bc3cb78898c5261ccfe83ff686e4a1
Brenner650 <41375066+Brenner650@users.noreply.github.com>
2020-12-07 20:10:52 +0100

    error correction
    error correction

--- CACCore.bat
+++ CACCore.bat
@@ -225,7 +225,7 @@ GOTO End
 %A1% -port=2602 "%ExileTanoa%%o1%%o2%%o4%"
 
 :Coop
-set Coop=-mod=Mods\@ace;Mods\@CBA_A3;Mods\@EnhancedMovement;Mods\@EnhancedMovementRework;Mods\@MfHealAbort;Mods\@Vindicta;Mods\@CHViewDistance;Mods\@VET_Unflipping%o3%
+set Coop=-mod=Mods\@ace;Mods\@CBA_A3;Mods\@EnhancedMovement;Mods\@EnhancedMovementRework;Mods\@MfHealAbort;Mods\@Vindicta;Mods\@CHViewDistance;Mods\@VET_Unflipping
 if %Status%==ENABLED goto CoopEXTENDED
 %A1% -port=2702 "%Coop%"
 GOTO End
@@ -415,7 +415,7 @@ GOTO End
 GOTO End
 
 :DominationEXTENDED_0
-%A1% -port=2342 "%Dominationn%%o4%%o3%%o2%%o1%"
+%A1% -port=2342 "%Domination%%o4%%o3%%o2%%o1%"
 GOTO End
 
 :Chernarus
