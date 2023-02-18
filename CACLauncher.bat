c190be9991531c647bb1f47a5e414678a49ff42c
TanRayCz <45832275+TanRayCz@users.noreply.github.com>
2023-02-18 15:04:02 +0100

    Hotfix: Update to address 0kb CACCore issue

--- CACLauncher.bat
+++ CACLauncher.bat
@@ -1,3 +1,3 @@
 @echo off
-curl -v --ssl-no-revoke https://raw.githubusercontent.com/TanRayCz/CAC/master/CACCore.bat > CACCore.bat 2> nul
+curl --ssl-no-revoke https://raw.githubusercontent.com/TanRayCz/CAC/master/CACCore.bat > CACCore.bat 2> nul
 call CACCore.bat
