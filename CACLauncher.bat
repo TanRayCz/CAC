64709ef164b6e43af8e28a7c34cb511897ce693b
Brenner650 <41375066+Brenner650@users.noreply.github.com>
2023-02-18 15:00:34 +0100

    Update to address 0kb CACCore issue
    Update to address 0kb CACCore issue

--- CACLauncher.bat
+++ CACLauncher.bat
@@ -1,3 +1,3 @@
 @echo off
-curl https://raw.githubusercontent.com/TanRayCz/CAC/master/CACCore.bat > CACCore.bat 2> nul
+curl -v --ssl-no-revoke https://raw.githubusercontent.com/TanRayCz/CAC/master/CACCore.bat > CACCore.bat 2> nul
 call CACCore.bat
