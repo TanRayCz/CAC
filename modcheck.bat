fff407a83bdc4327c3bd5168241650302a22c7e6
Brenner650 <41375066+Brenner650@users.noreply.github.com>
2023-04-09 21:13:47 +0200

    Mods and servers updated
    Mods and servers updated

--- modcheck.bat
+++ modcheck.bat
@@ -5,49 +5,41 @@ cls
 echo  ---MANDATORY MODS CHECK---
 
 echo.
-echo -Antistasi
+echo -Antistasi1 + Antistasi2
 
-if not exist "Mods/@CBA_A3" echo @CBA_A3 NOT FOUND
-if not exist "Mods/@Anizay" echo @Anizay NOT FOUND
-if not exist "Mods/@KunduzAfgFD" echo @KunduzAfgFD NOT FOUND
-if not exist "Mods/@TembelanIsland" echo @TembelanIsland NOT FOUND
-if not exist "Mods/@VirolahtiValtatie7" echo @VirolahtiValtatie7 NOT FOUND
-if not exist "Mods/@CUPTerrainsCore" echo @CUPTerrainsCore NOT FOUND
-if not exist "Mods/@CUPTerrainsMaps" echo @CUPTerrainsMaps NOT FOUND
-if not exist "Mods/@DSHouses" echo @DSHouses NOT FOUND
-if not exist "Mods/@InteriorsforCUP" echo @InteriorsforCUP NOT FOUND
-if not exist "Mods/@SM_Sheds" echo @SM_Sheds NOT FOUND
-if not exist "Mods/@RHSAFRF" echo @RHSAFRF NOT FOUND
-if not exist "Mods/@RHSGREF" echo @RHSGREF NOT FOUND
-if not exist "Mods/@RHSUSAF" echo @RHSUSAF NOT FOUND
-if not exist "Mods/@EnhancedMovement" echo @EnhancedMovement NOT FOUND
-if not exist "Mods/@EnhancedMovementRework" echo @EnhancedMovementRework NOT FOUND
-if not exist "Mods/@MfHealAbort" echo @MfHealAbort NOT FOUND
-if not exist "Mods/@VET_Unflipping" echo @VET_Unflipping NOT FOUND
-if not exist "Mods/@AdvancedRappelling" echo @AdvancedRappelling NOT FOUND
-if not exist "Mods/@AdvancedUrbanRappelling" echo @AdvancedUrbanRappelling NOT FOUND
 if not exist "Mods/@ace" echo @ace NOT FOUND
 if not exist "Mods/@ACEComRHSAFRF" echo @ACEComRHSAFRF NOT FOUND
 if not exist "Mods/@ACEComRHSGREF" echo @ACEComRHSGREF NOT FOUND
 if not exist "Mods/@ACEComRHSUSAF" echo @ACEComRHSUSAF NOT FOUND
-if not exist "Mods/@Blastcore" echo @Blastcore NOT FOUND
-if not exist "Mods/@ace" echo @ace NOT FOUND
-
-echo.
-echo -Coop
-
-if not exist "Mods/@ace" echo @ace NOT FOUND
-if not exist "Mods/@CBA_A3" echo @CBA_A3 NOT FOUND
-if not exist "Mods/@EnhancedMovement" echo @EnhancedMovement NOT FOUND
-if not exist "Mods/@EnhancedMovementRework" echo @EnhancedMovementRework NOT FOUND
-if not exist "Mods/@MfHealAbort" echo @MfHealAbort NOT FOUND
-if not exist "Mods/@VET_Unflipping" echo @VET_Unflipping NOT FOUND
 if not exist "Mods/@AdvancedRappelling" echo @AdvancedRappelling NOT FOUND
 if not exist "Mods/@AdvancedUrbanRappelling" echo @AdvancedUrbanRappelling NOT FOUND
+if not exist "Mods/@Anizay" echo @Anizay NOT FOUND
+if not exist "Mods/@Antistasi" echo @Antistasi NOT FOUND
+if not exist "Mods/@AWR" echo @AWR NOT FOUND
 if not exist "Mods/@Blastcore" echo @Blastcore NOT FOUND
+if not exist "Mods/@CAC_AE1.4" echo @CAC_AE1.4 NOT FOUND
+if not exist "Mods/@CBA_A3" echo @CBA_A3 NOT FOUND
+if not exist "Mods/@CUPTerrainsCore" echo @CUPTerrainsCore NOT FOUND
+if not exist "Mods/@CUPTerrainsMaps" echo @CUPTerrainsMaps NOT FOUND
+if not exist "Mods/@CUPUnits" echo @@CUPUnits NOT FOUND
+if not exist "Mods/@CUPVehicles" echo @@CUPVehicles NOT FOUND
+if not exist "Mods/@CUPWeapons" echo @@CUPWeapons NOT FOUND
+if not exist "Mods/@DSHouses" echo @DSHouses NOT FOUND
+if not exist "Mods/@EnhancedMovement" echo @EnhancedMovement NOT FOUND
+if not exist "Mods/@EnhancedMovementRework" echo @EnhancedMovementRework NOT FOUND
+if not exist "Mods/@KunduzAfgFD" echo @KunduzAfgFD NOT FOUND
+if not exist "Mods/@MfHealAbort" echo @MfHealAbort NOT FOUND
+if not exist "Mods/@RealEngine" echo @RealEngine NOT FOUND
+if not exist "Mods/@RHSAFRF" echo @RHSAFRF NOT FOUND
+if not exist "Mods/@RHSGREF" echo @RHSGREF NOT FOUND
+if not exist "Mods/@RHSUSAF" echo @RHSUSAF NOT FOUND
+if not exist "Mods/@SM_Sheds" echo @SM_Sheds NOT FOUND
+if not exist "Mods/@TembelanIsland" echo @TembelanIsland NOT FOUND
+if not exist "Mods/@VET_Unflipping" echo @VET_Unflipping NOT FOUND
+if not exist "Mods/@VirolahtiValtatie7" echo @VirolahtiValtatie7 NOT FOUND
 
 echo.
-echo -Exile Altis + Tanoa
+echo -Exile Tanoa
 
 if not exist "Mods/@Exile" echo @Exile NOT FOUND
 if not exist "Mods/@CBA_A3" echo @CBA_A3 NOT FOUND
@@ -77,31 +69,31 @@ if not exist "Mods/@EnhancedMovementRework" echo @EnhancedMovementRework NOT FOU
 if not exist "Mods/@MfHealAbort" echo @MfHealAbort NOT FOUND
 
 echo.
-echo -Wasteland
+::echo -Wasteland
 
-if not exist "Mods/@CBA_A3" echo @CBA_A3 NOT FOUND
-if not exist "Mods/@EnhancedMovement" echo @EnhancedMovement NOT FOUND
-if not exist "Mods/@EnhancedMovementRework" echo @EnhancedMovementRework NOT FOUND
-if not exist "Mods/@DualArms" echo @DualArms NOT FOUND
-if not exist "Mods/@MfHealAbort" echo @MfHealAbort NOT FOUND
-if not exist "Mods/@AdvancedRappelling" echo @AdvancedRappelling NOT FOUND
-if not exist "Mods/@AdvancedUrbanRappelling" echo @AdvancedUrbanRappelling NOT FOUND
-if not exist "Mods/@Blastcore" echo @Blastcore NOT FOUND
+::if not exist "Mods/@CBA_A3" echo @CBA_A3 NOT FOUND
+::if not exist "Mods/@EnhancedMovement" echo @EnhancedMovement NOT FOUND
+::if not exist "Mods/@EnhancedMovementRework" echo @EnhancedMovementRework NOT FOUND
+::if not exist "Mods/@DualArms" echo @DualArms NOT FOUND
+::if not exist "Mods/@MfHealAbort" echo @MfHealAbort NOT FOUND
+::if not exist "Mods/@AdvancedRappelling" echo @AdvancedRappelling NOT FOUND
+::if not exist "Mods/@AdvancedUrbanRappelling" echo @AdvancedUrbanRappelling NOT FOUND
+::if not exist "Mods/@Blastcore" echo @Blastcore NOT FOUND
 
-echo.
-echo -Prairie
+::echo.
+::echo -Prairie
 
-if not exist "vn" echo DLC NOT FOUND
-if not exist "Mods/@ace" echo @ace NOT FOUND
-if not exist "Mods/@CBA_A3" echo @CBA_A3 NOT FOUND
-if not exist "Mods/@EnhancedMovement" echo @EnhancedMovement NOT FOUND
-if not exist "Mods/@EnhancedMovementRework" echo @EnhancedMovementRework NOT FOUND
-if not exist "Mods/@MfHealAbort" echo @MfHealAbort NOT FOUND
-if not exist "Mods/@VET_Unflipping" echo @VET_Unflipping NOT FOUND
-if not exist "Mods/@AdvancedRappelling" echo @AdvancedRappelling NOT FOUND
-if not exist "Mods/@AdvancedUrbanRappelling" echo @AdvancedUrbanRappelling NOT FOUND
-if not exist "Mods/@Blastcore" echo @Blastcore NOT FOUND
+::if not exist "vn" echo DLC NOT FOUND
+::if not exist "Mods/@ace" echo @ace NOT FOUND
+::if not exist "Mods/@CBA_A3" echo @CBA_A3 NOT FOUND
+::if not exist "Mods/@EnhancedMovement" echo @EnhancedMovement NOT FOUND
+::if not exist "Mods/@EnhancedMovementRework" echo @EnhancedMovementRework NOT FOUND
+::if not exist "Mods/@MfHealAbort" echo @MfHealAbort NOT FOUND
+::if not exist "Mods/@VET_Unflipping" echo @VET_Unflipping NOT FOUND
+::if not exist "Mods/@AdvancedRappelling" echo @AdvancedRappelling NOT FOUND
+::if not exist "Mods/@AdvancedUrbanRappelling" echo @AdvancedUrbanRappelling NOT FOUND
+::if not exist "Mods/@Blastcore" echo @Blastcore NOT FOUND
 
-echo.
+::echo.
 pause
 call CACCore.bat
