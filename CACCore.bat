6e17ddd482eebe3ab0c97e3dbdfc2cc82e35cd1a
Brenner650 <41375066+Brenner650@users.noreply.github.com>
2023-09-20 21:11:01 +0200

    Ace compat mods for RHS added
    Ace compat mods for RHS added

--- CACCore.bat
+++ CACCore.bat
@@ -208,7 +208,7 @@ if %@JSRS_SOUNDMOD%==ENABLED set o2addon=;%ModPath%@JSRS_AFRF;%ModPath%@JSRS_GRE
 GOTO End
 
 :Antistasi2
-set Antistasi2=-mod=%ModPath%@CBA_A3;%ModPath%@Antistasi;%ModPath%@CAC_AE;%ModPath%@AWR;%ModPath%@RealEngine;%ModPath%@ace;%ModPath%@EnhancedMovement;%ModPath%@EnhancedMovementRework;%ModPath%@MfHealAbort;%ModPath%@VET_Unflipping;%ModPath%@AdvancedRappelling;%ModPath%@AdvancedUrbanRappelling;%ModPath%@Blastcore
+set Antistasi1=-mod=%ModPath%@CBA_A3;%ModPath%@ace;Mods\@ACEComRHSUSAF;Mods\@ACEComRHSAFRF;Mods\@ACEComRHSGREF;%ModPath%@Antistasi;%ModPath%@CAC_AE;%ModPath%@AWR;%ModPath%@RealEngine;%ModPath%@RHSUSAF;%ModPath%@RHSAFRF;%ModPath%@RHSGREF;%ModPath%@EnhancedMovement;%ModPath%@EnhancedMovementRework;%ModPath%@MfHealAbort;%ModPath%@VET_Unflipping;%ModPath%@AdvancedRappelling;%ModPath%@AdvancedUrbanRappelling
 if %Status%==ENABLED goto Antistasi2EXTENDED
 %A4% -port=2702 "%Antistasi2%"
 GOTO End
