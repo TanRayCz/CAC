e0165d8d0469677d886d0c4278c83b559baf558f
Brenner650 <41375066+Brenner650@users.noreply.github.com>
2024-01-18 21:19:04 +0100

    Removed not needed mods from A2
    Removed not needed mods from A2

--- CACCore.bat
+++ CACCore.bat
@@ -211,7 +211,7 @@ GOTO End
 GOTO End
 
 :Antistasi2
-set Antistasi2=-mod=spe;%ModPath%@CBA_A3;%ModPath%@KunduzAfgFD;%ModPath%@ace;%ModPath%@Antistasi;%ModPath%@CAC_AE;%ModPath%@IFA3;%ModPath%@AWR;%ModPath%@RealEngine;%ModPath%@CUPUnits;%ModPath%@CUPVehicles;%ModPath%@CUPWeapons;%ModPath%@EnhancedMovement;%ModPath%@EnhancedMovementRework;%ModPath%@MfHealAbort;%ModPath%@VET_Unflipping;%ModPath%@ace_nouniformrestrictions;%ModPath%@AdvancedRappelling;%ModPath%@AdvancedUrbanRappelling
+set Antistasi2=-mod=spe;%ModPath%@CBA_A3;%ModPath%@KunduzAfgFD;%ModPath%@ace;%ModPath%@Antistasi;%ModPath%@CAC_AE;%ModPath%@IFA3;%ModPath%@AWR;%ModPath%@RealEngine;%ModPath%@EnhancedMovement;%ModPath%@EnhancedMovementRework;%ModPath%@MfHealAbort;%ModPath%@VET_Unflipping;%ModPath%@ace_nouniformrestrictions;%ModPath%@AdvancedRappelling;%ModPath%@AdvancedUrbanRappelling
 %A4% -port=2702 "%Antistasi2%"
 GOTO End
 :Antistasi2EXTENDED
