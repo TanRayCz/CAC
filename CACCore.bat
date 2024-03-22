b76cf6de258a327d8257aaec494c0efdbe1d3e5a
Brenner650 <41375066+Brenner650@users.noreply.github.com>
2024-03-22 20:56:43 +0100

    Update CACCore.bat
    Mod settings numbers corrected

--- CACCore.bat
+++ CACCore.bat
@@ -219,7 +219,7 @@ if %Status%==ENABLED goto Antistasi2EXTENDED
 %A4% -port=2702 "%Antistasi2%"
 GOTO End
 :Antistasi2EXTENDED
-%A4% -port=2702 "%Antistasi2%%o1%%o2%%o3%%o4%%o5%"
+%A4% -port=2702 "%Antistasi2%%o1%%o2%%o3%%o4%%o5%%o6%"
 ::RHS::
 ::if %@JSRS_SOUNDMOD%==ENABLED set o2addon=;%ModPath%@JSRS_AFRF;%ModPath%@JSRS_GREF;%ModPath%@JSRS_USAF
 ::%A4% -port=3302 "%Antistasi2%%o1%%o2%%o2addon%%o3%%o4%%o5%"
@@ -308,9 +308,9 @@ if exist "Mods/@Blastcore " (echo  4 - @Blastcore 		                            
 if exist "Mods/@VanillaSmokeForBlastcore" (echo  5 - @VanillaSmokeForBlastcore - Blastcore required		STATUS: %@VanillaSmokeForBlastcore%) else echo  5 - @VanillaSmokeForBlastcore		                        STATUS: NOT FOUND
 if exist "Mods/@BetterInventory" (echo  6 - @BetterInventory	                                       STATUS: %@BetterInventory%) else echo  6 - @BetterInventory		                                STATUS: NOT FOUND
 echo.
-echo  6 - Return
+echo  7 - Return
 echo.
-echo  7 - Refresh
+echo  8 - Refresh
 echo.
 echo Confirm with enter
 SET /P "M=Switch optional mod:"
