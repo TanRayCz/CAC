5db92d56b318c07178401ce26c20b4252c476aaf
Brenner650 <41375066+Brenner650@users.noreply.github.com>
2024-08-31 02:10:47 +0200

    A2 map change
    Xulakia

--- CACCore.bat
+++ CACCore.bat
@@ -211,7 +211,7 @@ GOTO End
 GOTO End
 
 :Antistasi2
-set Antistasi2=-mod=RF;%ModPath%@CUPTerrainsCore;%ModPath%@Ruha;%ModPath%@CUPUnits;%ModPath%@CUPVehicles;%ModPath%@CUPWeapons;%ModPath%@ace;%ModPath%@CBA_A3;%ModPath%@AntistasiUltimate;%ModPath%@cac_aue;%ModPath%@AWR;%ModPath%@RealEngine;%ModPath%@EnhancedMovement;%ModPath%@EnhancedMovementRework;%ModPath%@VET_Unflipping;%ModPath%@ace_nouniformrestrictions;%ModPath%@AdvancedRappelling;%ModPath%@AdvancedUrbanRappelling;%ModPath%@ACEGrenades
+set Antistasi2=-mod=RF;%ModPath%@CUPTerrainsCore;%ModPath%@Yulakia;%ModPath%@CUPUnits;%ModPath%@CUPVehicles;%ModPath%@CUPWeapons;%ModPath%@ace;%ModPath%@CBA_A3;%ModPath%@AntistasiUltimate;%ModPath%@cac_aue;%ModPath%@AWR;%ModPath%@RealEngine;%ModPath%@EnhancedMovement;%ModPath%@EnhancedMovementRework;%ModPath%@VET_Unflipping;%ModPath%@ace_nouniformrestrictions;%ModPath%@AdvancedRappelling;%ModPath%@AdvancedUrbanRappelling;%ModPath%@ACEGrenades
 if %Status%==ENABLED goto Antistasi2EXTENDED
 %A2% -port=2702 "%Antistasi2%"
 GOTO End
